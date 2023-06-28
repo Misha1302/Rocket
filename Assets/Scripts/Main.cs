0f85d8eb6446751284e79b4441f3af296866f1bf
Михаил <misha13022008@gmail.com>
2023-06-28 19:55:17 +0300

    particles

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,10 +4,12 @@ public class Main : MonoBehaviour
 {
     [SerializeField] private RocketUiMovement rocketUiMovement;
     [SerializeField] private RocketMovement rocketMovement;
+    [SerializeField] private RocketParticlesManager particlesManager;
 
     private void Start()
     {
         ScreenManager.Init();
+        particlesManager.Init(rocketUiMovement);
         rocketMovement.Init(rocketUiMovement);
     }
 }
---
+++ Assets/Scripts/RocketParticlesManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RocketParticlesManager : MonoBehaviour
+{
+    public ParticleSystem particles;
+    private RocketUiMovement _uiMovement;
+
+    private void Update()
+    {
+        if (_uiMovement != null && _uiMovement.CanFlyUp)
+        {
+            if (!particles.isPlaying)
+                particles.Play();
+        }
+        else
+        {
+            if (particles.isPlaying)
+                particles.Stop();
+        }
+    }
+
+
+    public void Init(RocketUiMovement uiMovement)
+    {
+        _uiMovement = uiMovement;
+    }
+}
